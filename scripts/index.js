8a88a11cccc8951461d847793ab25d32bc013a0a
John Hutchins <johnwallshutchins@gmail.com>
2017-05-16 19:00:59 -0400

    slight style

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 ﻿var jqxhr = $.getJSON("products.json", function (product) {
 
+        //TODO adjust file to not use html elements
     $.each(product, function (key, value) {
         $("select").append("<option>" + value.name + " $" + value.price + "</option>");
     });
