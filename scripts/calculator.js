d33856966284a7fe6b34789e295358febea8beb6
John Hutchins <johnwallshutchins@gmail.com>
2017-05-16 18:37:46 -0400

    scripts + bootstrap

---
+++ scripts/calculator.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $("select").on("click", function () {
+        console.log("option clicked"); 
+        //calculate the total chosen here
+        //$("option").attr("class", "focus");
+    })
+});
---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+﻿var jqxhr = $.getJSON("products.json", function (product) {
+
+    $.each(product, function (key, value) {
+        //Do I even need the KEY in this example?
+        //$("tbody").append("<tr>" + "<td>" + value.name + ": $" + value.price + "</td>" + "</tr>");
+
+        //this should not use html in the javascript file. refactor
+        $("select").append("<option>" + value.name + " $" + value.price + "</option>");
+    });
+
+})
+    .fail(function (error) {
+        alert("Something went wrong, please re-load the page and try again. The Error Is: ", error);
+    });
