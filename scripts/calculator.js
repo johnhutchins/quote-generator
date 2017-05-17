22f331c0988ddc74c1ce4506e2184a71ee595caf
John Hutchins <johnwallshutchins@gmail.com>
2017-05-17 07:27:28 -0400

    scripts

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,8 @@
 ﻿$(function () {
     var total = 0,
-    selected_element = $("select option:selected").val();
+    selected_element = $("option:selected").val();
+
+
     
     console.log(selected_element, "is selected");
 });
