bbcda56047f8bbe2ec0f1127015f61835b3b0c1a
John Hutchins <johnwallshutchins@gmail.com>
2017-05-30 18:12:16 -0400

    progress towards table

---
+++ scripts/invoice.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    //need to add this function either to the html by calling it onClick,
+    //other option is to loop the click event.
+
+    var invoice = $("#invoice"),
+        product_chosen,
+        product_chosen_value = $("option").value,
+        product_for_deletion = $("tr"),
+        moveToTable,
+        removeFromTable,
+        newRow;
+
+    moveItemToTable = function () {
+        var row = invoice.insertRow();
+        
+        invoice.row.innerHTML = product_chosen_value;
+    };
+
+    removeItemFromTable = function () {
+        row_in_table.deleteRow();
+    };
+
+    addProductToTable = function () {
+        product_chosen.on("click", function () {
+            moveItemToTable();
+        });
+    };
+
+    removeProductFromTable = function () {
+        product_for_deletion.on("click", function () {
+            removeItemFromTable();
+        });
+    };
+
+    removeProductFromTable();
+    addProductToTable();
+});
